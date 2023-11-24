c664095cfe907d1b86d342a1884100daaa1ae93c
Counnselllor <sahiljagtap0013@gmail.com>
2023-11-24 18:24:01 +0530

    interview details

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,11 @@
 import Breadcrumb from '../components/Breadcrumb';
+import HrInterviewScheduling from '../components/InterviewSchedule';
 
 const Calendar = () => {
   return (
     <>
+        <HrInterviewScheduling />
+
       <Breadcrumb pageName="Calendar" />
 
       {/* <!-- ====== Calendar Section Start ====== --> */}
